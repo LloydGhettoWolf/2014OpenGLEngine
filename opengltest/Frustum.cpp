a2630f86598bfc025029d7ec00a6f01723803b28
GhettoWolf <lloyd.crawley@gmail.com>
2014-12-08 17:54:21 +0000

    changed frustum not to be object or class based

--- opengltest/Frustum.cpp
+++ opengltest/Frustum.cpp
@@ -4,57 +4,45 @@
 #pragma once
 #include "Frustum.h"
 
-Frustum* Frustum::GetInstance()
+void	NormalizePlane(Frustum& frustum, int index);
+
+void Extract(Frustum& frustum,const vec3& eye, const mat4x4& camMatrix, const mat4x4& projMatrix)
 {
-	if (!m_instance){
-		m_instance = new Frustum;
-		return m_instance;
-	}
+	frustum.m_pos = eye;
 
-	return m_instance;
-}
-
-
-void Frustum::Extract(const vec3& eye,const mat4x4& camMatrix,const mat4x4& projMatrix)
-{
-	m_pos = eye;
-
-	m_modView = camMatrix;
-	m_projMatrix = projMatrix;
-	mat4x4 MVPMatrix =  m_projMatrix *  m_modView;
+	mat4x4 MVPMatrix = projMatrix *  camMatrix;
 
 	MVPMatrix = glm::transpose(MVPMatrix);
 	//left and right planes
-	m_planes[0] = MVPMatrix[3] - MVPMatrix[0];
-	NormalizePlane(0);
+	frustum.m_planes[0] = MVPMatrix[3] - MVPMatrix[0];
+	NormalizePlane(frustum,0);
 
-	m_planes[1] = MVPMatrix[3] + MVPMatrix[0];
-	NormalizePlane(1);
+	frustum.m_planes[1] = MVPMatrix[3] + MVPMatrix[0];
+	NormalizePlane(frustum, 1);
 
 	//near and far planes
-	m_planes[2] = MVPMatrix[3] - MVPMatrix[2];
-	NormalizePlane(2);
+	frustum.m_planes[2] = MVPMatrix[3] - MVPMatrix[2];
+	NormalizePlane(frustum, 2);
 
-	m_planes[3] = MVPMatrix[3] + MVPMatrix[2];
-	NormalizePlane(3);
+	frustum.m_planes[3] = MVPMatrix[3] + MVPMatrix[2];
+	NormalizePlane(frustum, 3);
 
 	//top and bottom planes
-	m_planes[4] = MVPMatrix[3] - MVPMatrix[1];
-	NormalizePlane(4);
+	frustum.m_planes[4] = MVPMatrix[3] - MVPMatrix[1];
+	NormalizePlane(frustum, 4);
 
-	m_planes[5] = MVPMatrix[3] + MVPMatrix[1];
-	NormalizePlane(5);
+	frustum.m_planes[5] = MVPMatrix[3] + MVPMatrix[1];
+	NormalizePlane(frustum, 5);
 	
 }
 
 
+bool TestPoint(const Frustum& frustum,const vec3& testPoint){
 
-
-bool Frustum::TestPoint(const vec3& testPoint)const{
-
-	for(int plane = 0; plane < 6; plane++){
-		if(m_planes[plane].x * testPoint.x + m_planes[plane].y * testPoint.y + m_planes[plane].z * testPoint.z
-			+ m_planes[plane].w < 0.0f){
+	for(int planeNum = 0; planeNum < 6; planeNum++){
+		vec4 plane = frustum.m_planes[planeNum];
+		if (plane.x * testPoint.x + plane.y * testPoint.y + plane.z * testPoint.z
+			+ plane.w < 0.0f){
 			return false;
 		}
 	}
@@ -63,14 +51,14 @@ bool Frustum::TestPoint(const vec3& testPoint)const{
 }
 
 
-bool Frustum::TestSphere(const vec3& center, float radius) const
+bool TestSphere(const Frustum& frustum,const vec3& center, float radius)
 {
-	for(int plane = 0; plane < 6; plane++)
-	{
-		float t =	m_planes[plane][0] * center.x +
-					m_planes[plane][1] * center.y +
-					m_planes[plane][2] * center.z +
-					m_planes[plane][3];
+	for (int planeNum = 0; planeNum < 6; planeNum++){
+		vec4 plane = frustum.m_planes[planeNum];
+		float t = plane[0] * center.x +
+				  plane[1] * center.y +
+				  plane[2] * center.z +
+				  plane[3];
 
 		if( t < -radius)
 			return false;
@@ -82,7 +70,7 @@ bool Frustum::TestSphere(const vec3& center, float radius) const
 }
 
 
-bool Frustum::TestBox(const BoundingBox& bbox) const
+bool TestBox(const Frustum& frustum,const BoundingBox& bbox) 
 {
 	vec3 tCorners[8] = {	vec3(bbox.m_min.x, bbox.m_min.y, bbox.m_min.z),
 							vec3(bbox.m_max.x, bbox.m_min.y, bbox.m_min.z),
@@ -94,17 +82,17 @@ bool Frustum::TestBox(const BoundingBox& bbox) const
 							vec3(bbox.m_max.x, bbox.m_max.y, bbox.m_max.z)	};
 	int iTotalIn = 0;
 
-	for(int plane=0; plane<6; plane++)
-	{
+	for (int planeNum = 0; planeNum < 6; planeNum++){
+		vec4 plane = frustum.m_planes[planeNum];
 		int iInCount = 8;
 		int iPtIn = 1;
 
 		for(int corner = 0; corner < 8; corner++)
 		{
-			float side =	m_planes[plane][0] * tCorners[corner].x +
-							m_planes[plane][1] * tCorners[corner].y +
-							m_planes[plane][2] * tCorners[corner].z +
-							m_planes[plane][3];
+			float side =	plane[0] * tCorners[corner].x +
+							plane[1] * tCorners[corner].y +
+							plane[2] * tCorners[corner].z +
+							plane[3];
 			if(side < 0) {
 				iPtIn = 0;
 				iInCount--;
@@ -122,33 +110,32 @@ bool Frustum::TestBox(const BoundingBox& bbox) const
 }
 
 
-bool Frustum::CheckRect(const vec3& testPoint, float radius)
+bool CheckRect(const Frustum& frustum,const vec3& testPoint, float radius)
 {
 	  
-
 	    //2d check, so only need 4 planes and 4 points
-		for(int plane=0; plane<4; plane++) 
-		{
-			if(glm::dot(vec3(m_planes[plane]), vec3((testPoint.x - radius),0,(testPoint.z - radius)))
-				 + m_planes[plane].w > 0.0f)
+	for (int planeNum = 0; planeNum < 4; planeNum++){
+		vec4 plane = frustum.m_planes[planeNum];
+			if(glm::dot(vec3(plane), vec3((testPoint.x - radius),0,(testPoint.z - radius)))
+				 + plane.w > 0.0f)
 			{
 				continue;
 			}
 		
-			if(glm::dot(vec3(m_planes[plane]), vec3((testPoint.x + radius),0,(testPoint.z - radius)))
-				+ m_planes[plane].w > 0.0f)
+			if(glm::dot(vec3(plane), vec3((testPoint.x + radius),0,(testPoint.z - radius)))
+				+ plane.w > 0.0f)
 			{
 				continue;
 			}
 
-			if(glm::dot(vec3(m_planes[plane]), vec3((testPoint.x - radius),0, (testPoint.z + radius)))
-				+ m_planes[plane].w > 0.0f)
+			if(glm::dot(vec3(plane), vec3((testPoint.x - radius),0, (testPoint.z + radius)))
+				+ plane.w > 0.0f)
 			{
 				continue;
 			}
 
-			if(glm::dot(vec3(m_planes[plane]), vec3((testPoint.x + radius),0, (testPoint.z + radius)))
-				+ m_planes[plane].w > 0.0f)
+			if(glm::dot(vec3(plane), vec3((testPoint.x + radius),0, (testPoint.z + radius)))
+				+ plane.w > 0.0f)
 			{
 				continue;
 			}
@@ -157,5 +144,10 @@ bool Frustum::CheckRect(const vec3& testPoint, float radius)
 	return true;
 }
 
-
-Frustum* Frustum::m_instance = NULL;
+void  NormalizePlane(Frustum& frustum,int index){
+	vec4 plane = frustum.m_planes[index];
+	float normFactor = plane[0] * plane[0] + plane[1] * plane[1] +plane[2] * plane[2];
+	normFactor = sqrtf(normFactor);
+	plane /= normFactor;
+	frustum.m_planes[index] = plane;
+}
